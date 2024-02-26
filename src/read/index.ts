f7540d4193dc150a207603541aee78c911ecbb9d
Khánh Hoàng <hi@khanh.sh>
2024-02-26 13:20:31 +0700

    Update import path and simplify function documentation
    Streamlined code by updating the import path in 'src/index.ts' and 'src/read/index.ts'. Simplified the documentation for the function 'readVnNumber' by removing redundant lines and improving readability. Additionally, the configuration files 'deno.json' and 'package.json' have been updated to align with the changes.

--- src/read/index.ts
+++ src/read/index.ts
@@ -3,21 +3,15 @@ import NumberReader from './NumberReader.ts'
 /**
  * This is a helper that convert a number to a string like the way a real Vietnamese read it.
  *
- * This is a helper that convert a number to a string like the way a real Vietnamese read it.
- *
  * @example
  * ```ts
- * import { readVnNumber } from '@hckhanh/vn-number/read'
- * // import { readVnNumber } from 'jsr:@hckhanh/vn-number/read' // (for deno)
+ * import { readVnNumber } from '@hckhanh/vn-number'
  *
- * readVnNumber('19990000') // or readVnNumber(19990000)
- * // output: mười chín triệu chín trăm chín mươi nghìn
+ * readVnNumber('19990000') // or readVnNumber(19990000) // output: mười chín triệu chín trăm chín mươi nghìn
  * ```
  *
  * @param number the number to read. It can be string or number type
  * @return the Vietnamese number in string.
- *
- * @module
  */
 export function readVnNumber(number: string | number): string {
   return NumberReader.read(number)
