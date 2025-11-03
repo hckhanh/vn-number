6d6103830323d58e55a9e5421c6a74e498b7eade
Khánh Hoàng <hi@khanh.id>
2025-11-03 11:56:10 +0700

    Add `override` specifier and fix doc comments
    Added the `override` specifier to `read` methods in `Billion` and `Zerofill` classes for better clarity and adherence to TypeScript standards. Updated doc comments for consistency and clarity by correcting phrasing.

--- src/read/Billion.ts
+++ src/read/Billion.ts
@@ -1,10 +1,10 @@
 import Numbers from './Numbers.ts'
 
 /**
- * A group three numbers, a component in a billion position of the input number
+ * Group three numbers, a component in a billion position of the input number
  */
 export default class Billion extends Numbers {
-  public read(firstNumber?: boolean): string {
+  public override read(firstNumber?: boolean): string {
     return `${super.read(firstNumber)} tỷ`.trim()
   }
 }
