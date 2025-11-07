f73543f7dac37fa35b5b60617ace39dafcafd99b
Khánh Hoàng <hi@khanh.id>
2025-11-07 22:17:43 +0700

    Remove unused imports and adjust MDX component mapping
    Cleaned up unused imports from documentation files to streamline code and improve maintainability. Updated MDX component mapping in `mdx-components.tsx` to ensure proper integration of callout, card, and tab components. Adjusted CSS indentation for consistency.

--- docs/src/mdx-components.tsx
+++ docs/src/mdx-components.tsx
@@ -1,9 +1,16 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx'
 import type { MDXComponents } from 'mdx/types'
+import * as calloutComponents from 'fumadocs-ui/components/callout'
+import * as cardComponents from 'fumadocs-ui/components/card'
+import * as tabComponents from 'fumadocs-ui/components/tabs'
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...components,
+
+    ...calloutComponents,
+    ...cardComponents,
+    ...tabComponents,
   }
 }
